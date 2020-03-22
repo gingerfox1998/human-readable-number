beaf100dd359a1e9f55d140d6730caaf7ca723cd
gingerfox1998 <60549675+gingerfox1998@users.noreply.github.com>
2020-03-23 00:38:04 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ module.exports = function toReadable (number) {
     1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten', 11:'eleven', 12:'twelve', 
     13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eighteen', 19:'nineteen', 20:'twenty', 
     30:'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety', 100:'one hundred', 200:'two hundred',
-    300:'three hundred', 400:'four hundred'
+    300:'three hundred', 400:'four hundred', 500:'five hundred', 600:'six hundred', 700:'seven hundred', 800:'eight hundred',
+    900:'nine hundred'
   }
   var numstrarr = number.toString();
   if(numarr[number]) {
@@ -16,18 +17,12 @@ module.exports = function toReadable (number) {
     var tens = numarr[ten] + ' ' + numarr[numstrarr[1]];
     return tens;
   }
- if((number > 99) && (number < 1000)) {
-    var hundreds = numarr[numstrarr[0]] + ' hundred';
-    var tensforhund = number - numarr[numstrarr[0]] * 100;
-        if(numarr[tensforhund]) {
-            var newhundreds = hundreds + ' ' + numarr[tensforhund];
-            return newhundreds;
-        }
-        else if((tensforhund > 20) && (tensforhund < 100)) {
-            var ten = numstrarr[0] * 10;
-            var tens = numarr[ten] + ' ' + numarr[numstrarr[1]];
-            hundreds = hundreds + ' ' + tens;
-            return hundreds;
-        }
+ else if((number > 99) && (number < 1000)) {
+    var hundred = numstrarr[0] * 100;
+    var withouthund = number - hundred;
+    if(numarr[withouthund]) {
+        var hundreds = numarr[hundred] + ' ' + numarr[withouthund];
+        return hundreds;
     }
+ }
 }
